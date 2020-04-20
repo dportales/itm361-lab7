18126e15129e04664b65deb11f8fb9d5d7295391
dportales <59981221+dportales@users.noreply.github.com>
2020-04-20 15:27:27 -0500

    fix naming

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 function init(){
 //add your javascrip between these two lines of code
-	var in = document.getElementById();
-	var button = document.getElementById();
-	var out = document.getElementById();
+	var input = document.getElementById(entryinput);
+	var button = document.getElementById(entrybutton);
+	var output = document.getElementById(textoutput);
 	button.addEventListener('click' , function(){
 		out.innerHTML = in.value;
 		alert("David Portales: " + in.value)
