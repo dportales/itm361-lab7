40dd5c438740b3afc495279691fbb7c210edc934
dportales <59981221+dportales@users.noreply.github.com>
2020-04-20 15:38:19 -0500

    fixing

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 function init(){
 //add your javascrip between these two lines of code
-	var input = document.getElementById(entryinput);
-	var button = document.getElementById(entrybutton);
-	var output = document.getElementById(textoutput);
+	var input = document.getElementById('entryinput');
+	var button = document.getElementById('entrybutton');
+	var output = document.getElementById('textoutput');
 	button.addEventListener('click' , function(){
-		out.innerHTML = in.value;
-		alert("David Portales: " + in.value)
+		output.innerHTML = input.value;
+		alert("David Portales: " + input.value)
 	)};
 }
 window.addEventListener('load', init);
